e697a080206960e5c70482943b2c842ffb696a0e
Alan Russell <ajrussellaudio@gmail.com>
2016-09-23 11:41:05 +0100

    implement store tests

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -3,10 +3,18 @@ var Record = require( "../record" );
 
 describe("Record Store", function() {
 
-  it("has a name");
+  var recordStore = new RecordStore( "Big Al's Record Store", "Glasgow" );
 
-  it("is in a city");
+  it("has a name", function() {
+    assert.equal( "Big Al's Record Store", recordStore.name );
+  });
 
-  it("is empty at start");
+  it("is in the best city on planet earth", function() {
+    assert.equal( "Glasgow", recordStore.location );
+  });
+
+  it("is empty at start", function() {
+    assert.deepEqual( [], recordStore.inventory );
+  });
 
 })
