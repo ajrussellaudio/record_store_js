0d7a51238069b50475b523cc42181d116872a83b
Alan Russell <ajrussellaudio@gmail.com>
2016-09-23 11:28:56 +0100

    add store specs

---
+++ specs/record_store_spec.js
@@ -0,0 +1,12 @@
+var assert = require( "assert" );
+var Record = require( "../record" );
+
+describe("Record Store", function() {
+
+  it("has a name");
+
+  it("is in a city");
+
+  it("is empty at start");
+
+})
